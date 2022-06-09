82904bca11cbd67f6ecb63a1bae7851ec7a4e67c
DadiT <HackerSantochi@gmail.com>
2022-06-09 21:20:58 +0100

    date to arabic format

--- src/components/routes/Feed/Product.js
+++ src/components/routes/Feed/Product.js
@@ -1,11 +1,11 @@
 import { connect } from "react-redux";
 import { createProduct } from "../../../actions";
 import { useNavigate } from "react-router-dom";
-
 import { useTranslation } from "react-i18next";
 function Product({ product, createProduct, channelName }) {
   const navigate = useNavigate();
   const { t } = useTranslation();
+
   return (
     <div className="flex sm:flex-col border-[#EAEAEA] border-[0.5px] px-2  py-2 ">
       <section
@@ -54,7 +54,12 @@ function Product({ product, createProduct, channelName }) {
           <li>
             <p className="text-[#717171] text-sm">{`${new Date(
               product["date"]
-            ).toDateString()}`}</p>
+            ).toLocaleDateString("ar-EG", {
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}`}</p>
           </li>
         </ul>
         {/* <i className="self-end cursor-pointer text-[#545454] border-[#b5b5b5] hover:text-main mb-4 inline-flex justify-center items-center w-[28px] h-[28px] rounded-full border-[0.5px] hover:border-main  active:text-main active:border-main ">
