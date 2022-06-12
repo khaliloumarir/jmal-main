3bb9ee54fdf896f011f7b5276099bcd45ab769bf
DadiT <HackerSantochi@gmail.com>
2022-06-12 14:18:45 +0100

    added 404 page

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
-
+import Page404 from "./routes/Page404";
 //i18
 i18n
   .use(initReactI18next)
@@ -92,6 +92,7 @@ ReactDOM.render(
                   </AuthIsRequired>
                 }
               />
+              <Route path="*" element={<Page404 />} />
               <Route
                 path="product"
                 element={
--- src/routes/Page404.js
+++ src/routes/Page404.js
@@ -1,9 +1,21 @@
 import { useTranslation } from "react-i18next";
+import image from "./assets/images/404.svg";
+import { Link } from "react-router-dom";
+import "./assets/css/style.css";
 export default function Page404() {
   const { t } = useTranslation();
   return (
     <div>
-      <h1>{t("404_page")}</h1>
+      {/* <h1>{t("404_page")}</h1> */}
+      <div class="container">
+        <img class="mx-auto h-[400px]" src={image} />
+        <br />
+        <h3>{t("404_page")}</h3>
+        <br />
+        <Link to="/">
+          <button class="hover:opacity-75">{t("get_back")}</button>
+        </Link>
+      </div>
     </div>
   );
 }
